c14647d118ef8dd1669890894e8a19de33131971
nlutz <nlutz@mit.edu>
2017-05-20 14:13:22 -0400

    sketch

---
+++ DataGame/sketch.js
@@ -0,0 +1,18 @@
+var intro;
+
+function setup() {
+  for(var i = 0; i<table.getRowCount; i++){
+    beginShape();
+    if (shapeid[i-1] == shapeid[i]){
+      //adding vertices
+    }
+    else{
+      endShape();
+    }
+    
+  }
+}
+
+function draw() {
+  
+}
